2e7a9b2b77b4531ce4e7c1e2e83ef309496ea0cd
AlexEntrepreneur <hello@alexentrepreneur.com>
2019-10-15 00:14:09 +0100

    .

--- script.js
+++ script.js
@@ -1,48 +1,7 @@
 //====== Lexical Scope & Closure ======//
-// The World: (Global)
-let totalPopulation = "2bn";
-
-function kingdom() {
-  const queen = "Queen Ruby";
-  console.log(`Long live ${queen}`);
-
-  function city() {
-    const mayor = "Mayor Jim";
-    console.log(`${queen} demands more taxes!`);
-
-    function town() {
-      const townCrier = "Crier Tom";
-      console.log(`${townCrier} has a message for the town: ${queen} demands more taxes! And ${mayor} demands half of your crops!`);
-    }
-    town();
-  }
-  city();
-}
-
-kingdom();
-
 
 //====== Higher Order Functions ======//
-function compute(param1, param2, callbackFunction) {
-  return callbackFunction(param1, param2);
-}
 
-// callbacks
-function add(a, b) {
-  return a + b;
-}
-
-function subtract(a, b) {
-  return a - b;
-}
-
-function multiply(a, b) {
-  return a * b;
-}
-
-console.log(compute(2, 5, add));
-console.log(compute(543, 23, multiply));
-console.log(compute(32, 16, subtract));
 
 //====== Array Methods ======//
 const cityData = [
@@ -53,30 +12,9 @@ const cityData = [
 ];
 
 // filter
-let filteredCities = cityData.filter(function (currentItem) {
-  return currentItem.population > 650000;
-});
-
-console.log(filteredCities);
 
 // Map
-const lowerCaseCity = cityData.map(currentItem => currentItem.city.toLowerCase());
-
-console.log(lowerCaseCity);
 
 // forEach
-const populationDoom = [];
-
-cityData.forEach(function (currentItem) {
-  currentItem.population = 0;
-  populationDoom.push(currentItem);
-});
-
-console.log(populationDoom);
 
 // Reduce
-const totalCityLandArea = cityData.reduce((acc, city) => {
-  return acc + city.land_area;
-}, 0);
-
-console.log(totalCityLandArea);
